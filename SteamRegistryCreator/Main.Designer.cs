18c3ec8bc2495cc846c08e32ebf63061f59f30f1
Franck Dechavanne <rallymen007@gmail.com>
2015-02-23 21:41:48 +0100

    Initial commit

---
+++ SteamRegistryCreator/Main.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SteamRegistryCreator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bSelectFolder = new System.Windows.Forms.Button();
+            this.lFolderName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.CheckedListBox();
+            this.bCreateEntries = new System.Windows.Forms.Button();
+            this.dFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.bClearList = new System.Windows.Forms.Button();
+            this.bSelectAll = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bSelectFolder);
+            this.groupBox1.Controls.Add(this.lFolderName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(646, 44);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // bSelectFolder
+            // 
+            this.bSelectFolder.Location = new System.Drawing.Point(517, 11);
+            this.bSelectFolder.Name = "bSelectFolder";
+            this.bSelectFolder.Size = new System.Drawing.Size(97, 23);
+            this.bSelectFolder.TabIndex = 2;
+            this.bSelectFolder.Text = "Select folder...";
+            this.bSelectFolder.UseVisualStyleBackColor = true;
+            this.bSelectFolder.Click += new System.EventHandler(this.bSelectFolder_Click);
+            // 
+            // lFolderName
+            // 
+            this.lFolderName.AutoSize = true;
+            this.lFolderName.Location = new System.Drawing.Point(156, 16);
+            this.lFolderName.Name = "lFolderName";
+            this.lFolderName.Size = new System.Drawing.Size(84, 13);
+            this.lFolderName.TabIndex = 1;
+            this.lFolderName.Text = "Select a folder...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selected Steam library:";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(10, 62);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(645, 274);
+            this.listBox.TabIndex = 1;
+            // 
+            // bCreateEntries
+            // 
+            this.bCreateEntries.Location = new System.Drawing.Point(10, 377);
+            this.bCreateEntries.Name = "bCreateEntries";
+            this.bCreateEntries.Size = new System.Drawing.Size(644, 29);
+            this.bCreateEntries.TabIndex = 2;
+            this.bCreateEntries.Text = "Create Registry entries";
+            this.bCreateEntries.UseVisualStyleBackColor = true;
+            this.bCreateEntries.Click += new System.EventHandler(this.bCreateEntries_Click);
+            // 
+            // bClearList
+            // 
+            this.bClearList.Location = new System.Drawing.Point(10, 342);
+            this.bClearList.Name = "bClearList";
+            this.bClearList.Size = new System.Drawing.Size(315, 29);
+            this.bClearList.TabIndex = 3;
+            this.bClearList.Text = "Clear list";
+            this.bClearList.UseVisualStyleBackColor = true;
+            this.bClearList.Click += new System.EventHandler(this.clearList);
+            // 
+            // bSelectAll
+            // 
+            this.bSelectAll.Location = new System.Drawing.Point(339, 342);
+            this.bSelectAll.Name = "bSelectAll";
+            this.bSelectAll.Size = new System.Drawing.Size(315, 29);
+            this.bSelectAll.TabIndex = 4;
+            this.bSelectAll.Text = "Select all";
+            this.bSelectAll.UseVisualStyleBackColor = true;
+            this.bSelectAll.Click += new System.EventHandler(this.selectAll);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 418);
+            this.Controls.Add(this.bSelectAll);
+            this.Controls.Add(this.bClearList);
+            this.Controls.Add(this.bCreateEntries);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Steam Registry Entries Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bSelectFolder;
+        private System.Windows.Forms.Label lFolderName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox listBox;
+        private System.Windows.Forms.Button bCreateEntries;
+        private System.Windows.Forms.FolderBrowserDialog dFolder;
+        private System.Windows.Forms.Button bClearList;
+        private System.Windows.Forms.Button bSelectAll;
+    }
+}
+
---
+++ SteamRegistryCreator/Main.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace SteamRegistryCreator
+{
+    public partial class Main : Form
+    {
+        private String Gpath;
+        private List<Game> games;
+
+        private delegate void AddGameHandler(Int32 id, String name, String path);
+        private event AddGameHandler AddGame;
+
+        public Main()
+        {
+            // Admin rights or not?
+            if (!new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                // not an admin
+                MessageBox.Show("You need to run this program as administrator", "Error: requires admin rights", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            InitializeComponent();
+            games = new List<Game>();
+            AddGame += addGame;
+        }
+
+        private void bSelectFolder_Click(object sender, EventArgs e)
+        {
+            clearList(null, null);
+
+            dFolder.ShowDialog();
+            Gpath = dFolder.SelectedPath;
+            lFolderName.Text = Gpath;
+
+            Thread w = new Thread(new ThreadStart(refreshGameList));
+            w.Start();
+        }
+
+        private void addGame(Int32 id, String name, String path)
+        {
+            if (listBox.InvokeRequired)
+            {
+                AddGameHandler lba = new AddGameHandler(addGame);
+                this.Invoke(lba, new object[] { id, name, path });
+            }
+            else
+            {
+                games.Add(new Game(id, name, path));
+                listBox.Items.Add(name + "\t[" + Gpath + "]");
+            }
+        }
+
+        private delegate void DisableButtonHandler();
+        private void disableButton()
+        {
+            if (bCreateEntries.InvokeRequired)
+            {
+                DisableButtonHandler b = new DisableButtonHandler(disableButton);
+                this.Invoke(b, null);
+            }
+            else
+            {
+                bClearList.Enabled = false;
+                bSelectAll.Enabled = false;
+                bCreateEntries.Enabled = false;
+                bCreateEntries.Text = "Creating entries...";
+            }
+        }
+
+        private delegate void EnableButtonHandler();
+        private void enableButton()
+        {
+            if (bCreateEntries.InvokeRequired)
+            {
+                EnableButtonHandler b = new EnableButtonHandler(enableButton);
+                this.Invoke(b, null);
+            }
+            else
+            {
+                bClearList.Enabled = true;
+                bSelectAll.Enabled = true;
+                bCreateEntries.Enabled = true;
+                bCreateEntries.Text = "Create Registry entries";
+            }
+        }
+
+        private Boolean isLibrary()
+        {
+            if(Directory.GetDirectories(Gpath, "SteamApps").Length == 1){
+                Gpath +=  @"\SteamApps";
+                return true;
+            } else if(Directory.GetDirectories(Gpath, "common").Length == 1 && Directory.GetDirectories(Gpath, "downloading").Length == 1){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private void refreshGameList()
+        {
+            char[] sep = new char[]{'\t'};
+            RegistryKey uninstallRoot = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", true);
+            if (isLibrary())
+            {
+                // It looks like a steam library
+                String[] files = Directory.GetFiles(Gpath, "*.acf");
+                foreach(String f in files){
+                    Int32 id=0; String name=null, path=null, line;
+                    StreamReader sr = new StreamReader(f);
+                    while((line = sr.ReadLine()) != null){
+                        line = line.Trim().Replace("\"", "");
+                        if (line.StartsWith("appid"))
+                        {
+                            string[] w= line.Split(sep);
+                            id = Int32.Parse(line.Split(sep)[2]);
+                            continue;
+                        }
+                        if (line.StartsWith("name"))
+                        {
+                            name = line.Split(sep)[2];
+                            continue;
+                        }
+                        if (line.StartsWith("installdir"))
+                        {
+                            path = Gpath + @"\common\" + line.Split(sep)[2];
+                            continue;
+                        }
+                    }
+                    if(uninstallRoot.OpenSubKey("Steam App " + id) == null){
+                        // Only add games with no key
+                        AddGame(id, name, path);
+                    }
+                }
+            }
+        }
+        
+        private void bCreateEntries_Click(object sender, EventArgs e)
+        {
+            disableButton();
+            List<Game> selectedGames = new List<Game>();
+            foreach(Int32 index in listBox.CheckedIndices){
+                selectedGames.Add(games[index]);
+            }
+
+            Thread w = new Thread(()=>createEntries(selectedGames));
+            w.Start();
+        }
+
+        private void createEntries(List<Game> selectedGames)
+        {
+            // Get path to Steam.exe
+            String steamPath = (String) Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Valve\Steam", false).GetValue("InstallPath");
+            RegistryKey uninstallRoot = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", true);
+            foreach (Game game in selectedGames)
+            {
+                // Make sure the key isn't already there
+                if (uninstallRoot.OpenSubKey("Steam App " + game.id) == null)
+                {
+                    RegistryKey reg = uninstallRoot.CreateSubKey("Steam App " + game.id);
+                    reg.SetValue("DisplayIcon", steamPath + @"\Steam.exe");
+                    reg.SetValue("DisplayName", game.name);
+                    reg.SetValue("HelpLink", "http://support.steampowered.com");
+                    reg.SetValue("InstallLocation", game.path);
+                    reg.SetValue("NoModify", 1);
+                    reg.SetValue("NoRepair", 1);
+                    reg.SetValue("Publisher", "Unknown");
+                    reg.SetValue("UninstallString", "\"" + steamPath + "\\Steam.exe\" steam://uninstall/" + game.id);
+                    reg.SetValue("URLInfoAbout", "http://store.steampowered.com/app/" + game.id);
+                }
+            }
+
+            enableButton();
+            MessageBox.Show("Registry entries created", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void selectAll(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                listBox.SetItemCheckState(i, CheckState.Checked);
+            }
+        }
+
+        private void clearList(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            games.Clear();
+        }
+    }
+
+    public class Game
+    {
+        public Int32 id { get; set; }
+        public String name { get; set; }
+        public String path { get; set; }
+
+        public Game(Int32 i, String n, String p) { id = i; name = n; path = p; }
+    }
+}
